0c41a9a705233d285469bfc1b358a5574a7a5c1f
Abhiraj <195514@nith.ac.in>
2022-12-24 18:13:02 +0530

    Final Commit

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from core.views import Home
+from core.views import Home, ProfileList, ProfileCreate
 
 app_name = 'core'
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('profile/', ProfileList.as_view(), name='profile_list'),
+    path('profile/create/', ProfileCreate.as_view(), name='profile_create'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.utils.decorators import method_decorator
 from django.views import View
 
 
 class Home(View):
     def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('core:profile_list')
         return render(request, 'index.html')
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileList(View):
+    def get(self, request, *args, **kwargs):
+        profiles = request.user.profiles.all()
+        return render(request, 'profileList.html', {'profiles': profiles})
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileCreate(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'profileCreate.html')
